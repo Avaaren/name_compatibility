5cb60e4827a91b045e25ef465e983c199d96fe46
Avaaren <avaaren@mail.ru>
2020-03-23 20:40:04 +0300

    registered male and female models on admin site

--- predict/admin.py
+++ predict/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
-from .models import Relationship
+from .models import Relationship, MaleName, FemaleName
 
 @admin.register(Relationship)
 class RelationshipAdmin(admin.ModelAdmin):
     list_display = ['male_name', 'female_name']
+
+@admin.register(MaleName)
+class MaleNameAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(FemaleName)
+class FemaleNameAdmin(admin.ModelAdmin):
+    list_display = ['name']
