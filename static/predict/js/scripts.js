b461eae2df2f15174f42d1d2826c27f27c58356d
Avaaren <avaaren@mail.ru>
2020-03-24 12:58:38 +0300

    Added ajax search for both sex names and click handling for result

---
+++ static/predict/js/scripts.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    $('#man-search-field').on('input', function (e) {
+        $.ajax({
+            url: "ajax_search_male/",
+            data: {
+                'search_request': $(this).val(),
+            },
+            dataType: 'json',
+            success: function (data) {
+                $('#result_man').empty();
+                if (data.result_set.length == 0) {
+                    $('#result_man').append($('<p>').text('empty'));
+                }
+                else {
+                    data.result_set.forEach(function (item) {
+                        var p = $('<p>', { 'class': 'result' }).text(item);
+                        $('#result_man').append(p);
+                    });
+                }
+            }
+        });
+
+    });
+
+    $('#woman-search-field').on('input', function (e) {
+        $.ajax({
+            url: "ajax_search_female/",
+            data: {
+                'search_request': $(this).val(),
+            },
+            dataType: 'json',
+            success: function (data) {
+                $('#result_woman').empty();
+                if (data.result_set.length == 0) {
+                    $('#result_woman').append($('<p>').text('empty'));
+                }
+                else {
+                    data.result_set.forEach(function (item) {
+                        var p = $('<p>', { 'class': 'result' }).text(item);
+                        $('#result_woman').append(p);
+                    });
+                }
+            }
+        });
+
+    });
+    $(document).on('click', 'p.result', function(){
+        value = $(this).text();
+        if ($(this).parent().attr('id')=='result_man'){
+            $('#man-search-field').val(value);
+            $('#result_man').empty();
+        }
+        else{
+            $('#woman-search-field').val(value);
+            $('#result_woman').empty();
+        }
+    });
+});
