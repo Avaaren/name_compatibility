507a393733e45ebbac589efd179454dce829a9e5
Avaaren <avaaren@mail.ru>
2020-03-24 12:54:24 +0300

    Added urls for ajax man and woman search

--- predict/urls.py
+++ predict/urls.py
@@ -7,4 +7,6 @@ app_name = 'predict'
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     # path('<slug:name_slug>/', views.PredictView.as_view(), name='predict')
+    path('ajax_search_male/', views.ajax_search_male, name='search_male'),
+    path('ajax_search_female/', views.ajax_search_female, name='search_female'),
 ]
