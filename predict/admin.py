59453d2a98f2ae8438be50539b7b61337ed7d3c9
Avaaren <avaaren@mail.ru>
2020-04-03 15:33:11 +0300

    Added description field to admin panel

--- predict/admin.py
+++ predict/admin.py
@@ -4,7 +4,7 @@ from .models import Relationship, MaleName, FemaleName
 
 @admin.register(Relationship)
 class RelationshipAdmin(admin.ModelAdmin):
-    list_display = ['male_name', 'female_name']
+    list_display = ['male_name', 'female_name', 'description']
 
 @admin.register(MaleName)
 class MaleNameAdmin(admin.ModelAdmin):
