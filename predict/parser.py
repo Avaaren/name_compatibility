7d33d745db8a848457e8160edd02075bff0f344d
Avaaren <avaaren@mail.ru>
2020-04-03 15:17:15 +0300

    Reworked parser.Now writing data to csv and it works =)

--- predict/parser.py
+++ predict/parser.py
@@ -1,8 +1,9 @@
 import requests
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 from pytils.translit import slugify
 from datetime import datetime
 from multiprocessing import Pool
+import csv
 
 
 def benchmark(func):
@@ -12,6 +13,7 @@ def benchmark(func):
         print(datetime.now() - start)
     return wrapper
 
+
 def get_names():
     url = 'https://goroskop365.ru/sovmestimost-imen/agnessa-averyan/'
 
@@ -20,8 +22,6 @@ def get_names():
     result = []
     man_values = []
     woman_values = []
-    
-
 
     women_names_list = soup.find('select', {'name': 'female_name'})
     women_names_list = women_names_list.find_all('option')
@@ -31,7 +31,6 @@ def get_names():
             woman_values.append(woman_name['value'])
         except KeyError:
             continue
-
 
     men_names_list = soup.find('select', {'name': 'male_name'})
     men_names_list = men_names_list.find_all('option')
@@ -52,6 +51,7 @@ def get_names():
             result.append(total_values)
     return result
 
+
 def get_urls():
     url_list = []
     names_list = get_names()
@@ -60,84 +60,92 @@ def get_urls():
         url_list.append(url)
     return url_list
 
-responses = []
-def get_responses(first, last):
-    
-    url_list = get_names()
-    for url in url_list[first:last]:
-        URL = f'https://goroskop365.ru/sovmestimost-imen/{url[2]}/'
-        response = requests.get(URL)
-        responses.append(response)
-    
-# def threading_urls():
-#     thread_list = []
-#     first = 0
-#     length = 18762
 
-#     for i in range(3):
-#         if length < 2000:
-#             t = Thread(target=get_responses, args=(first, first+length))
-#         else:
-#             t = Thread(target=get_responses, args=(first, first+2000))
-#         thread_list.append(t)
-#         t.start()
-#         first += 2000
-#         length -= 2000
+def get_response(url):
+    response = requests.get(url)
+    return response
 
-#     for thread in thread_list:
-#         thread.join()
-#     # print(responses)
-#     return responses
 
-@benchmark
-def get_relationship_type():
-    counter = 1
+def get_page_data(response):
     page_info = []
-    relationships = []
+    if response.status_code != 200:
+        pass
+    else:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        header = soup.find('div', class_='sovmestimost').find('br').text
 
-    # responses_list = threading_urls()
-    for response in responses_list:
-        try:
-            if response.status_code != 200:
+        percent_text = soup.find('div', class_='compatibility-percentage')
+        percent_text = percent_text.text
+        percent_text = percent_text.split('\n')
+
+        percent_in_love = percent_text[1].split(
+            '–')[1].strip().split('%')[0].strip()
+        percent_in_marriage = percent_text[2].split(
+            '–')[1].strip().split('%')[0].strip()
+
+        header_with_names = soup.find_all('h2')[1].text.strip()
+        header_with_names = header_with_names.split(' ')
+
+        male_name = header_with_names[::-1][0]
+        female_name = header_with_names[::-1][2]
+        slug = slugify(f'{female_name}-{male_name}')
+
+        page_info.append(female_name)
+        page_info.append(male_name)
+        page_info.append(slug)
+        page_info.append(percent_in_love)
+        page_info.append(percent_in_marriage)
+        page_info.append(header)
+
+        body = soup.find('div', class_='sovmestimost')
+        for s in body.next_siblings:
+            if s.name == None:
                 continue
+            elif s.name == 'p':
+                page_info.append(s.text)
             else:
-                
-                soup = BeautifulSoup(response.text, 'html.parser')
-                header = soup.find('div', class_='sovmestimost').find('br').text
+                break
 
-                percent_text = soup.find('div', class_='compatibility-percentage')
-                percent_text = percent_text.text
-                percent_text = percent_text.split('\n')
+        descr = ''
+        for string in page_info[6:]:
+            descr += string
 
-                percent_in_love = percent_text[1].split('–')[1].strip().split('%')[0].strip()
-                percent_in_marriage = percent_text[2].split('–')[1].strip().split('%')[0].strip()
+        page_data = {
+            'female_name': page_info[0],
+            'male_name': page_info[1],
+            'slug': page_info[2],
+            'percent_in_love': page_info[3],
+            'percent_in_married': page_info[4],
+            'header': page_info[5],
+            'description': descr
+        }
+        return page_data
 
-                header_with_names = soup.find_all('h2')[1].text.strip()
-                header_with_names = header_with_names.split(' ')
 
-                male_name = header_with_names[::-1][0]
-                female_name = header_with_names[::-1][2]
-                slug = slugify(f'{female_name}-{male_name}')
+def write_csv(page_data):
+    with open('relationships.csv', 'a') as f:
+        writer = csv.writer(f)
+        try:
+            writer.writerow((
+                page_data['female_name'],
+                page_data['male_name'],
+                page_data['slug'],
+                page_data['percent_in_love'],
+                page_data['percent_in_married'],
+                page_data['header'],
+                page_data['description'])
+            )
+            print(page_data['slug'])
+        except:
+            pass
 
-                page_info.append(female_name)
-                page_info.append(male_name)
-                page_info.append(slug)
-                page_info.append(percent_in_love)
-                page_info.append(percent_in_marriage)
-                page_info.append(header)
 
-                body = soup.find('div', class_='sovmestimost')
-                for s in body.next_siblings:
-                    if s.name == None:
-                        continue
-                    elif s.name == 'p':
-                        page_info.append(s.text)
-                    else:
-                        break
-                
-                relationships.append(page_info)
-                counter += 1
-        except Exception as exception:
-            print(type(exception).__name__)
-            continue
-    return relationships
+def write_all_to_csv(url):
+    response = get_response(url)
+    page_data = get_page_data(response)
+    write_csv(page_data)
+
+
+url_list = get_urls()
+with Pool(40) as pool:
+    pool.map(write_all_to_csv, url_list)
